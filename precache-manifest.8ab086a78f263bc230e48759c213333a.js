c36af6a6c45d0c44ba65c0dbed7e246e17bdfd9d
sstojanovic <sstojanovic@transperfect.com>
2020-01-09 13:39:29 +0100

    Updates

---
+++ precache-manifest.8ab086a78f263bc230e48759c213333a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23577da5fb1b122dd1fdcfa19593741e",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "8f803ef2cc371ad3644f",
+    "url": "/react-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a3e89b657e5f97d35ed1",
+    "url": "/react-app/static/js/2.310f69b6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-app/static/js/2.310f69b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f803ef2cc371ad3644f",
+    "url": "/react-app/static/js/main.30dd41e7.chunk.js"
+  },
+  {
+    "revision": "ee2707fefec4dd08d7c4",
+    "url": "/react-app/static/js/runtime-main.59528cf0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
